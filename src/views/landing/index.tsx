f0a9ecc80e0de178c495f8b3da39c7ae52ba390e
Noah Hollowell <noahdhollowell@googlemail.com>
2021-02-27 11:46:57 +0000

    Basic Tailwind css

--- src/views/landing/index.tsx
+++ src/views/landing/index.tsx
@@ -1,9 +1,25 @@
 import { FC } from 'react'
+import { Link } from 'react-router-dom';
 
 const Landing: FC = () => {
     return (
         <>
-            <p>landing</p>
+            <div className="relative bg-white overflow-hidden h-screen">
+                <div className="max-w-7xl mx-auto h-full flex items-center justify-center">
+                    
+                        <div className="text-center flex flex-col items-center">
+                            <h1 className="text-9xl font-bold text-purple-500 pb-4">LectureMon</h1>
+                            <p className="text-lg font-bold">Because we are way better than you will ever be!</p>
+
+                            <div className="flex mt-8">
+                                <Link to="login" className="shadow rounded-md py-1 px-4 text-xl hover:bg-gray-100 mr-10">Login</Link>
+                                <Link to="register" className="shadow rounded-md py-1 px-4 text-xl hover:bg-gray-100">Register</Link>
+                            </div>
+
+                        </div>
+                    
+                </div>
+            </div>
         </>
     )
 }
